f11cc33e7d9fd9cb895ca93572ea679459ef3407
bolodev <bolodevel@gmail.com>
2015-05-09 15:02:51 +0100

    Updated to include OSX Lion (10.7)

--- plugins/osx/BluetoothPlist.py
+++ plugins/osx/BluetoothPlist.py
@@ -252,6 +252,14 @@ class BluetoothPlist(Plugin):
                     plist = ccl_bplist.load(bplist)
                     bplist.close()
                     try:
+                        # PairedDevices ARRAY
+                        if "PairedDevices" in plist:
+                            if len(plist["PairedDevices"]) == 0:
+                                of.write("Paired Devices:\r\n\tNo paired devices listed.\r\n")
+                            else:
+                                of.write("Paired Devices:\r\n")
+                                for paired_device in plist["PairedDevices"]:
+                                    of.write("\tDevice: {}\r\n".format(paired_device))
                         # D2D MAC Address
                         if "D2D MAC Address" in plist:
                             of.write("D2D MAC Address :{}\r\n".format(plist["D2D MAC Address"]))
@@ -337,9 +345,106 @@ class BluetoothPlist(Plugin):
                     of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
                     print("[WARNING] File: {} does not exist or cannot be found.".format(file))    
             elif self._os_version == "lion":
-                logging.info("This version of OSX is not supported by this plugin.")
-                print("[INFO] This version of OSX is not supported by this plugin.")
-                of.write("[INFO] This version of OSX is not supported by this plugin.\r\n")
+                if os.path.isfile(file):
+                    bplist = open(file, "rb")
+                    plist = ccl_bplist.load(bplist)
+                    bplist.close()
+                    try:
+                        # D2D MAC Address
+                        # if "D2D MAC Address" in plist:
+                        #     of.write("D2D MAC Address :{}\r\n".format(plist["D2D MAC Address"]))
+
+                        # DaemonControllersConfigurationKey
+                        if "DaemonControllersConfigurationKey" in plist:
+                            of.write("Daemon Controllers Configuration Key:\r\n")
+                            for dmcck_key in plist["DaemonControllersConfigurationKey"]:
+                                # INNER DICT
+                                of.write("\t{}\r\n".format(dmcck_key))
+                                for item in plist["DaemonControllersConfigurationKey"][dmcck_key]:
+                                    of.write("\t\t{}: {}\r\n".format(item, plist["DaemonControllersConfigurationKey"][dmcck_key][item]))
+                        # PANInterfaces ARRAY
+                        if "PANInterfaces" in plist:
+                            of.write("PAN Interfaces:\r\n")
+                            for pan_interface in plist["PANInterfaces"]:
+                                of.write("\tPAN Interface: {}\r\n".format(pan_interface))
+                        # ControllerPowerState
+                        if "ControllerPowerState" in plist:
+                            of.write("Controller Power State: {}\r\n".format(plist["ControllerPowerState"]))
+                        # HIDDevices ARRAY
+                        if "HIDDevices" in plist:
+                            of.write("HIDDevices\r\n")
+                            for hid_device in plist["HIDDevices"]:
+                                of.write("\tHID Device: {}\r\n".format(hid_device))
+                        # DeviceCache
+                        if "DeviceCache" in plist:
+                            of.write("Device Cache\r\n")
+                            for cached_device in plist["DeviceCache"]:
+                                # MAC ADDRESS
+                                of.write("\tCached Device: {}\r\n".format(cached_device))
+                                for device_data in plist["DeviceCache"][cached_device]:
+                                    # Name
+                                    if "Name" in device_data:
+                                        of.write("\t\tName: {}\r\n".format(plist["DeviceCache"][cached_device]["Name"]))
+                                    # Manufacturer
+                                    if "Manufacturer" in device_data:
+                                        of.write("\t\tManufacturer: {}\r\n".format(plist["DeviceCache"][cached_device]["Manufacturer"]))
+                                    # ClassOfDevice
+                                    if "ClassOfDevice" in device_data:
+                                        of.write("\t\tClass Of Device: {}\r\n".format(plist["DeviceCache"][cached_device]["ClassOfDevice"]))
+                                    # BatteryPercent
+                                    if "BatteryPercent" in device_data:
+                                        of.write("\t\tBattery Percent: {}\r\n".format(plist["DeviceCache"][cached_device]["BatteryPercent"]))
+                                    # ClockOffset
+                                    if "ClockOffset" in device_data:
+                                        of.write("\t\tClock Offset: {}\r\n".format(plist["DeviceCache"][cached_device]["ClockOffset"]))
+                                    # LastNameUpdate
+                                    if "LastNameUpdate" in device_data:
+                                        of.write("\t\tLast Name Update: {}\r\n".format(plist["DeviceCache"][cached_device]["LastNameUpdate"]))
+                                    # LastServicesUpdate
+                                    if "LastServicesUpdate" in device_data:
+                                        of.write("\t\tLast Services Update: {}\r\n".format(plist["DeviceCache"][cached_device]["LastServicesUpdate"]))
+                                    # LastInquiryUpdate
+                                    if "LastInquiryUpdate" in device_data:
+                                        of.write("\t\tLast Inquiry Update: {}\r\n".format(plist["DeviceCache"][cached_device]["LastInquiryUpdate"]))
+                                    # LMPVersion
+                                    if "LMPVersion" in device_data:
+                                        of.write("\t\tLMP Version: {}\r\n".format(plist["DeviceCache"][cached_device]["LMPVersion"]))
+                                    # LMPSubversion
+                                    if "LMPSubversion" in device_data:
+                                        of.write("\t\tLMP Subversion: {}\r\n".format(plist["DeviceCache"][cached_device]["LMPSubversion"]))
+                                    # InquiryRSSI
+                                    if "InquiryRSSI" in device_data:
+                                        of.write("\t\tInquiry RSSI: {}\r\n".format(plist["DeviceCache"][cached_device]["InquiryRSSI"]))
+                                    # PageScanMode
+                                    if "PageScanMode" in device_data:
+                                        of.write("\t\tPage Scan Mode: {}\r\n".format(plist["DeviceCache"][cached_device]["PageScanMode"]))
+                                    # PageScanPeriod
+                                    if "PageScanPeriod" in device_data:
+                                        of.write("\t\tPage Scan Period: {}\r\n".format(plist["DeviceCache"][cached_device]["PageScanPeriod"]))
+                                    # PageScanRepetitionMode
+                                    if "PageScanRepetitionMode" in device_data:
+                                        of.write("\t\tPage Scan Repetition Mode: {}\r\n".format(plist["DeviceCache"][cached_device]["PageScanRepetitionMode"]))
+                                    # EIRData NOT PARSED
+                                    # Services NOT PARSED
+                        # PersistentPorts NOT PARSED
+                        # PairedDevices ARRAY
+                        if "PairedDevices" in plist:
+                            if len(plist["PairedDevices"]) == 0:
+                                of.write("Paired Devices:\r\n\tNo paired devices listed.\r\n")
+                            else:
+                                of.write("Paired Devices:\r\n")
+                                for paired_device in plist["PairedDevices"]:
+                                    of.write("\tDevice: {}\r\n".format(paired_device))
+                    except KeyError:
+                        pass
+                    of.write("\r\n")
+                else:
+                    logging.warning("File: {} does not exist or cannot be found.".format(file))
+                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {} does not exist or cannot be found.".format(file))
+                # logging.info("This version of OSX is not supported by this plugin.")
+                # print("[INFO] This version of OSX is not supported by this plugin.")
+                # of.write("[INFO] This version of OSX is not supported by this plugin.\r\n")
             elif self._os_version == "snow_leopard":
                 logging.info("This version of OSX is not supported by this plugin.")
                 print("[INFO] This version of OSX is not supported by this plugin.")
