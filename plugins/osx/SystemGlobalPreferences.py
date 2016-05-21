f9ce81ef67ba26fa68bd1e1cf3d6ba7f1b514d9f
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:50:26 +0100

    Code clean - positional parameters in string format

--- plugins/osx/SystemGlobalPreferences.py
+++ plugins/osx/SystemGlobalPreferences.py
@@ -32,72 +32,72 @@ class SystemGlobalPreferences(Plugin):
         with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             global_plist = os.path.join(self._input_dir, "Library", "Preferences", self._data_file)
-            of.write("Source File: {}\r\n\r\n".format(global_plist))
+            of.write("Source File: {0}\r\n\r\n".format(global_plist))
             if self._os_version in ["el_capitan", "yosemite", "mavericks", "mountain_lion", "lion"]:
                 if os.path.isfile(global_plist):
                     bplist = open(global_plist, "rb")
                     plist = ccl_bplist.load(bplist)
                     bplist.close()
                     if "MultipleSessionEnabled" in plist:
-                        of.write("Multiple Session Enabled       : {}\r\n".format(plist["MultipleSessionEnabled"]))
+                        of.write("Multiple Session Enabled       : {0}\r\n".format(plist["MultipleSessionEnabled"]))
                     if "com.apple.updatesettings_did_disable_ftp" in plist:
-                        of.write("Update Settings Did Disable FTP: {}\r\n"
+                        of.write("Update Settings Did Disable FTP: {0}\r\n"
                                  .format(plist["com.apple.updatesettings_did_disable_ftp"]))
                     if "com.apple.AppleModemSettingTool.LastCountryCode" in plist:
-                        of.write("Modem Last Country Code        : {}\r\n"
+                        of.write("Modem Last Country Code        : {0}\r\n"
                                  .format(plist["com.apple.AppleModemSettingTool.LastCountryCode"]))
                     if "Country" in plist:
-                        of.write("Country                        : {}\r\n".format(plist["Country"]))
+                        of.write("Country                        : {0}\r\n".format(plist["Country"]))
                     if "AppleLocale" in plist:
-                        of.write("Apple Locale                   : {}\r\n".format(plist["AppleLocale"]))
+                        of.write("Apple Locale                   : {0}\r\n".format(plist["AppleLocale"]))
                     if "AppleLanguages" in plist:
                         apple_languages = plist["AppleLanguages"]
                         of.write("Languages:\r\n")
                         for apple_language in apple_languages:
                             of.write("\tLanguage: {}\r\n".format(apple_language))
                 else:
-                    logging.warning("File {} does not exist.".format(global_plist))
-                    print("[WARNING] File {} does not exist.".format(global_plist))
+                    logging.warning("File {0} does not exist.".format(global_plist))
+                    print("[WARNING] File {0} does not exist.".format(global_plist))
             elif self._os_version == "snow_leopard":
                 if os.path.isfile(global_plist):
                     bplist = open(global_plist, "rb")
                     plist = ccl_bplist.load(bplist)
                     bplist.close()
                     if "com.apple.AppleModemSettingTool.LastCountryCode" in plist:
-                        of.write("Last Country Code: {}\r\n"
+                        of.write("Last Country Code: {0}\r\n"
                                  .format(plist["com.apple.AppleModemSettingTool.LastCountryCode"]))
                     if "com.apple.preferences.timezone.selected_city" in plist:
-                        of.write("Selected City: {}\r\n")
+                        of.write("Selected City: {0}\r\n")
                         selected_city = plist["com.apple.preferences.timezone.selected_city"]
                         if "RegionalCode" in selected_city:
-                            of.write("\tRegional Code : {}\r\n".format(selected_city["RegionalCode"]))
+                            of.write("\tRegional Code : {0}\r\n".format(selected_city["RegionalCode"]))
                         if "Version" in selected_city:
-                            of.write("\tVersion       : {}\r\n".format(selected_city["Version"]))
+                            of.write("\tVersion       : {0}\r\n".format(selected_city["Version"]))
                         if "TimeZoneName" in selected_city:
-                            of.write("\tTime Zone Name: {}\r\n".format(selected_city["TimeZoneName"]))
+                            of.write("\tTime Zone Name: {0}\r\n".format(selected_city["TimeZoneName"]))
                         if "Latitude" in selected_city:
-                            of.write("\tLatitude      : {}\r\n".format(selected_city["Latitude"]))
+                            of.write("\tLatitude      : {0}\r\n".format(selected_city["Latitude"]))
                         if "Longitude" in selected_city:
-                            of.write("\tLongitude     : {}\r\n".format(selected_city["Longitude"]))
+                            of.write("\tLongitude     : {0}\r\n".format(selected_city["Longitude"]))
                         if "GeonameID" in selected_city:
-                            of.write("\tGeoname ID    : {}\r\n".format(selected_city["GeonameID"]))
+                            of.write("\tGeoname ID    : {0}\r\n".format(selected_city["GeonameID"]))
                         if "CountryCode" in selected_city:
-                            of.write("\tCountry Code  : {}\r\n".format(selected_city["CountryCode"]))
+                            of.write("\tCountry Code  : {0}\r\n".format(selected_city["CountryCode"]))
                         if "Name" in selected_city:
-                            of.write("\tName          : {}\r\n".format(selected_city["Name"]))
+                            of.write("\tName          : {0}\r\n".format(selected_city["Name"]))
 
                     if "Country" in plist:
-                        of.write("Country: {}\r\n".format(plist["Country"]))
+                        of.write("Country: {0}\r\n".format(plist["Country"]))
                     if "AppleLocale" in plist:
-                        of.write("Apple Locale: {}\r\n".format(plist["AppleLocale"]))
+                        of.write("Apple Locale: {0}\r\n".format(plist["AppleLocale"]))
                     if "AppleLanguages" in plist:
                         of.write("Apple Languages:\r\n")
                         for apple_language in plist["AppleLanguages"]:
-                            of.write("\t{}\r\n".format(apple_language))
+                            of.write("\t{0}\r\n".format(apple_language))
                     if "com.apple.TimeZonePref.Last_Selected_City" in plist:
                         of.write("Last Selected City:\r\n")
                         for item in plist["com.apple.TimeZonePref.Last_Selected_City"]:
-                            of.write("\t{}\r\n".format(item))
+                            of.write("\t{0}\r\n".format(item))
             else:
                 logging.warning("Not a known OSX version.")
                 of.write("[WARNING] Not a known OSX version.\r\n")
