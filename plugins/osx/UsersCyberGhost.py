2ba8ceff58ac2b93d7375cafde62aa3daa340c84
bolodev <bolodev@users.noreply.github.com>
2016-05-21 21:08:41 +0100

    Code clean - positional parameters in string format

--- plugins/osx/UsersCyberGhost.py
+++ plugins/osx/UsersCyberGhost.py
@@ -40,8 +40,8 @@ class UsersCyberGhost(Plugin):
                     if os.path.isfile(config):
                         self.__parse_plist(config, username)
                     else:
-                        logging.warning("{} does not exist.".format(config))
-                        print("[WARNING] {} does not exist.".format(config))
+                        logging.warning("{0} does not exist.".format(config))
+                        print("[WARNING] {0} does not exist.".format(config))
         else:
             print("[WARNING] {} does not exist.".format(users_path))
             
@@ -52,41 +52,41 @@ class UsersCyberGhost(Plugin):
         with codecs.open(os.path.join(self._output_dir, "Users_" + username + "_VPN_CyberGhost.txt"), "a",
                          encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-            of.write("Source File: {}\r\n\r\n".format(file))
+            of.write("Source File: {0}\r\n\r\n".format(file))
             if os.path.isfile(file):
                 bplist = open(file, "rb")
                 pl = ccl_bplist.load(bplist)
                 bplist.close()
                 try:
                     if "Cyberghost_GUID" in pl:
-                        of.write("Cyberghost GUID      : {}\r\n".format(pl["Cyberghost_GUID"]))
+                        of.write("Cyberghost GUID      : {0}\r\n".format(pl["Cyberghost_GUID"]))
                     if "startAtSystemStart" in pl:
-                        of.write("Start at System Start: {}\r\n".format(pl["startAtSystemStart"]))
+                        of.write("Start at System Start: {0}\r\n".format(pl["startAtSystemStart"]))
                     if "SULastCheckTime" in pl:
-                        of.write("SU Last Check Time   : {}\r\n".format(pl["SULastCheckTime"]))
+                        of.write("SU Last Check Time   : {0}\r\n".format(pl["SULastCheckTime"]))
                     if "RandomPort" in pl:
-                        of.write("Random Port          : {}\r\n".format(pl["RandomPort"]))
+                        of.write("Random Port          : {0}\r\n".format(pl["RandomPort"]))
                     if "useOpenVpnOverTcp" in pl:
-                        of.write("Use OpenVpn Over Tcp : {}\r\n".format(pl["useOpenVpnOverTcp"]))
+                        of.write("Use OpenVpn Over Tcp : {0}\r\n".format(pl["useOpenVpnOverTcp"]))
                     if "Cyberghost_RemoteSettings" in pl:
                         of.write("Cyberghost Remote Settings:\r\n")
                         remote_settings = pl["Cyberghost_RemoteSettings"]
                         # instdate -> Unix Millisecond
                         date_foo = remote_settings["instdate"]
                         date_foo = datetime.datetime.fromtimestamp(date_foo/1000.0)
-                        of.write("\tInstall Date           : {}\r\n".format(date_foo))
-                        of.write("\tLast Country           : {}\r\n".format(remote_settings["LastCountry"]))
-                        of.write("\tLast Server            : {}\r\n".format(remote_settings["LastServer"]))
-                        of.write("\tLast Logged In         : {}\r\n".format(remote_settings["LastLoggedIn"]))
-                        of.write("\tLas tPlan ID           : {}\r\n".format(remote_settings["LastPlanID"]))
-                        of.write("\tENC Connected Server ID: {}\r\n".format(remote_settings["ENC_ConnectedServerID"]))
-                        of.write("\tLast Plan Name         : {}\r\n".format(remote_settings["LastPlanName"]))
-                        of.write("\tStart Counter          : {}\r\n".format(remote_settings["startCounter"]))
+                        of.write("\tInstall Date           : {0}\r\n".format(date_foo))
+                        of.write("\tLast Country           : {0}\r\n".format(remote_settings["LastCountry"]))
+                        of.write("\tLast Server            : {0}\r\n".format(remote_settings["LastServer"]))
+                        of.write("\tLast Logged In         : {0}\r\n".format(remote_settings["LastLoggedIn"]))
+                        of.write("\tLas tPlan ID           : {0}\r\n".format(remote_settings["LastPlanID"]))
+                        of.write("\tENC Connected Server ID: {0}\r\n".format(remote_settings["ENC_ConnectedServerID"]))
+                        of.write("\tLast Plan Name         : {0}\r\n".format(remote_settings["LastPlanName"]))
+                        of.write("\tStart Counter          : {0}\r\n".format(remote_settings["startCounter"]))
                 except KeyError:
                     pass
             else:
-                logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                print("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
+                logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
             of.write("="*40 + "\r\n\r\n")
         of.close()
