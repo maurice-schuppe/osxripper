6e54b1edc53d5e82ef217add85fdc11cddda71e9
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:41:22 +0100

    Code clean - positional parameters in string format

--- plugins/osx/NetworkInterfaces.py
+++ plugins/osx/NetworkInterfaces.py
@@ -33,7 +33,7 @@ class NetworkInterfaces(Plugin):
         with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             plist_file = os.path.join(self._input_dir, "Library", "Preferences", "SystemConfiguration", self._data_file)
-            of.write("Source File: {}\r\n\r\n".format(plist_file))
+            of.write("Source File: {0}\r\n\r\n".format(plist_file))
             if self._os_version in ["el_capitan", "yosemite", "mavericks"]:
                 if os.path.isfile(plist_file):
                     with open(plist_file, "rb") as pl:
@@ -43,41 +43,42 @@ class NetworkInterfaces(Plugin):
                             network_interfaces = plist["Interfaces"]
                             for network_interface in network_interfaces:
                                 if "Active" in network_interface:
-                                    of.write("Active                   : {}\r\n".format(network_interface["Active"]))
+                                    of.write("Active                   : {0}\r\n".format(network_interface["Active"]))
                                 if "BSD Name" in network_interface:
-                                    of.write("BSD Name                 : {}\r\n".format(network_interface["BSD Name"]))
+                                    of.write("BSD Name                 : {0}\r\n".format(network_interface["BSD Name"]))
                                 if "IOBuiltin" in network_interface:
-                                    of.write("IOBuiltin                : {}\r\n".format(network_interface["IOBuiltin"]))
+                                    of.write("IOBuiltin                : {0}\r\n"
+                                             .format(network_interface["IOBuiltin"]))
                                 if "IOInterfaceNamePrefix" in network_interface:
-                                    of.write("IO Interface Name Prefix : {}\r\n"
+                                    of.write("IO Interface Name Prefix : {0}\r\n"
                                              .format(network_interface["IOInterfaceNamePrefix"]))
                                 if "IOInterfaceType" in network_interface:
-                                    of.write("IO Interface Type        : {}\r\n"
+                                    of.write("IO Interface Type        : {0}\r\n"
                                              .format(network_interface["IOInterfaceType"]))
                                 if "IOInterfaceUnit" in network_interface:
-                                    of.write("IO Interface Unit        : {}\r\n"
+                                    of.write("IO Interface Unit        : {0}\r\n"
                                              .format(network_interface["IOInterfaceUnit"]))
                                 if "IOMACAddress" in network_interface:
-                                    of.write("IO MAC Address           : {}\r\n"
+                                    of.write("IO MAC Address           : {0}\r\n"
                                              .format(binascii.hexlify(network_interface["IOMACAddress"])))
                                 if "SCNetworkInterfaceInfo" in network_interface:
-                                    of.write("SC Network Interface Info: {}\r\n"
+                                    of.write("SC Network Interface Info: {0}\r\n"
                                              .format(network_interface["SCNetworkInterfaceInfo"]["UserDefinedName"]))
                                 if "SCNetworkInterfaceType" in network_interface:
-                                    of.write("SC Network Interface Type: {}\r\n"
+                                    of.write("SC Network Interface Type: {0}\r\n"
                                              .format(network_interface["SCNetworkInterfaceType"]))
                                 of.write("\r\n")
 ################################
 # Move to own Plugin?
                         if "Model" in plist:
-                            of.write("Model: {}\r\n".format(plist["Model"]))
+                            of.write("Model: {0}\r\n".format(plist["Model"]))
 ################################
                     except KeyError:
                         pass
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(plist_file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(plist_file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(plist_file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(plist_file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(plist_file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(plist_file))
             elif self._os_version in ["mountain_lion", "lion", "snow_leopard"]:
                 if os.path.isfile(plist_file):
                     with open(plist_file, "rb") as pl:
@@ -87,38 +88,39 @@ class NetworkInterfaces(Plugin):
                             network_interfaces = plist["Interfaces"]
                             for network_interface in network_interfaces:
                                 if "Active" in network_interface:
-                                    of.write("Active                   : {}\r\n".format(network_interface["Active"]))
+                                    of.write("Active                   : {0}\r\n".format(network_interface["Active"]))
                                 if "BSD Name" in network_interface:
-                                    of.write("BSD Name                 : {}\r\n".format(network_interface["BSD Name"]))
+                                    of.write("BSD Name                 : {0}\r\n".format(network_interface["BSD Name"]))
                                 if "IOBuiltin" in network_interface:
-                                    of.write("IOBuiltin                : {}\r\n".format(network_interface["IOBuiltin"]))
+                                    of.write("IOBuiltin                : {0}\r\n"
+                                             .format(network_interface["IOBuiltin"]))
                                 if "IOInterfaceType" in network_interface:
-                                    of.write("IO Interface Type        : {}\r\n"
+                                    of.write("IO Interface Type        : {0}\r\n"
                                              .format(network_interface["IOInterfaceType"]))
                                 if "IOInterfaceUnit" in network_interface:
-                                    of.write("IO Interface Unit        : {}\r\n"
+                                    of.write("IO Interface Unit        : {0}\r\n"
                                              .format(network_interface["IOInterfaceUnit"]))
                                 if "IOMACAddress" in network_interface:
-                                    of.write("IO MAC Address           : {}\r\n"
+                                    of.write("IO MAC Address           : {0}\r\n"
                                              .format(binascii.hexlify(network_interface["IOMACAddress"])))
                                 if "SCNetworkInterfaceInfo" in network_interface:
-                                    of.write("SC Network Interface Info: {}\r\n"
+                                    of.write("SC Network Interface Info: {0}\r\n"
                                              .format(network_interface["SCNetworkInterfaceInfo"]["UserDefinedName"]))
                                 if "SCNetworkInterfaceType" in network_interface:
-                                    of.write("SC Network Interface Type: {}\r\n"
+                                    of.write("SC Network Interface Type: {0}\r\n"
                                              .format(network_interface["SCNetworkInterfaceType"]))
                                 of.write("\r\n")
 ################################
 # Move to own Plugin?
                         if "Model" in plist:
-                            of.write("Model: {}\r\n".format(plist["Model"]))
+                            of.write("Model: {0}\r\n".format(plist["Model"]))
 ################################
                     except KeyError:
                         pass
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(plist_file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(plist_file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(plist_file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(plist_file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(plist_file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(plist_file))
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
