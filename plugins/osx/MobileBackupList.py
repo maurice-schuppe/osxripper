f8ce75fa752e3625b964979e48e4378cb11fc7c7
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:40:12 +0100

    Code clean - positional parameters in string format

--- plugins/osx/MobileBackupList.py
+++ plugins/osx/MobileBackupList.py
@@ -31,19 +31,19 @@ class MobileBackupList(Plugin):
             mobilebackups_dir = os.path.join(self._input_dir, ".MobileBackups")
             if os.path.isdir(mobilebackups_dir):
                 of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-                of.write("Source Directory: {}\r\n\r\n".format(mobilebackups_dir))
+                of.write("Source Directory: {0}\r\n\r\n".format(mobilebackups_dir))
                 file_listing = os.listdir(mobilebackups_dir)
                 for f in file_listing:
-                    of.write("\t{}\r\n".format(f))
+                    of.write("\t{0}\r\n".format(f))
                     test_path = os.path.join(mobilebackups_dir, f)
                     if os.path.isdir(test_path):
                         test_path_file_list = os.listdir(test_path)
                         for test_path_file in test_path_file_list:
-                            of.write("\t\t{}\r\n".format(test_path_file))
+                            of.write("\t\t{0}\r\n".format(test_path_file))
             else:
-                logging.warning("Directory {} does not exist.".format(mobilebackups_dir))
-                of.write("[WARNING] Directory {} does not exist or cannot be found.\r\n".format(mobilebackups_dir))
-                print("[WARNING] Directory {} does not exist.".format(mobilebackups_dir))
+                logging.warning("Directory {0} does not exist.".format(mobilebackups_dir))
+                of.write("[WARNING] Directory {0} does not exist or cannot be found.\r\n".format(mobilebackups_dir))
+                print("[WARNING] Directory {0} does not exist.".format(mobilebackups_dir))
 
             of.write("="*40 + "\r\n\r\n")
         of.close()
