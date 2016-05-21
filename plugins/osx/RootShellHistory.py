85c182aac6689dcb7cd0f230226629d1dd96c7d4
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:46:51 +0100

    Code clean - positional parameters in string format

--- plugins/osx/RootShellHistory.py
+++ plugins/osx/RootShellHistory.py
@@ -33,18 +33,18 @@ class RootShellHistory(Plugin):
             with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
                 of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
                 file = os.path.join(root_path, self._data_file)
-                of.write("Source File: {}\r\n\r\n".format(file))
+                of.write("Source File: {0}\r\n\r\n".format(file))
                 if os.path.isfile(file):
                     history_file = codecs.open(file, "r", encoding="utf-8")
                     for lines in history_file:
                         of.write(lines.replace("\n", "\r\n"))
                     history_file.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
                 of.write("="*40 + "\r\n\r\n")
             of.close()
         else:
-            print("[WARNING] {} does not exist.".format(root_path))
-            logging.warning("{} does not exist.".format(root_path))
+            print("[WARNING] {0} does not exist.".format(root_path))
+            logging.warning("{0} does not exist.".format(root_path))
