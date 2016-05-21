0ad1ce16b5e3b366768792f56782aabdd6d421e7
bolodev <bolodev@users.noreply.github.com>
2016-05-21 21:21:06 +0100

    Code clean - positional parameters in string format

--- plugins/osx/UsersSafariCache.py
+++ plugins/osx/UsersSafariCache.py
@@ -38,11 +38,11 @@ class UsersSafariCache(Plugin):
                     if os.path.isfile(sqlite_db):
                         self.__parse_sqlite_db(sqlite_db, username)
                     else:
-                        logging.warning("{} does not exist.".format(sqlite_db))
-                        print("[WARNING] {} does not exist.".format(sqlite_db))
+                        logging.warning("{0} does not exist.".format(sqlite_db))
+                        print("[WARNING] {0} does not exist.".format(sqlite_db))
         else:
-            logging.warning("{} does not exist.".format(users_path))
-            print("[WARNING] {} does not exist.".format(users_path))
+            logging.warning("{0} does not exist.".format(users_path))
+            print("[WARNING] {0} does not exist.".format(users_path))
     
     def __parse_sqlite_db(self, file, username):
         """
@@ -54,7 +54,7 @@ class UsersSafariCache(Plugin):
             if self._os_version in ["el_capitan", "yosemite", "mavericks", "mountain_lion"]:
                 query = "SELECT request_key, time_stamp FROM cfurl_cache_response"
                 if os.path.isfile(file):
-                    of.write("Source File: {}\r\n\r\n".format(file))
+                    of.write("Source File: {0}\r\n\r\n".format(file))
                     conn = None
                     try:
                         conn = sqlite3.connect(file)
@@ -66,7 +66,7 @@ class UsersSafariCache(Plugin):
                                 if row[0] is None:
                                     of.write("Request Key:\r\n")
                                 else:
-                                    of.write("Request Key: {}\r\n".format(row[0]))
+                                    of.write("Request Key: {0}\r\n".format(row[0]))
                                 # if row[1] is None:
                                 #     of.write("Partition  :\r\n")
                                 # else:
@@ -74,24 +74,24 @@ class UsersSafariCache(Plugin):
                                 if row[1] is None:
                                     of.write("Timestamp  :\r\n")
                                 else:
-                                    of.write("Timestamp  : {}\r\n".format(row[1]))
+                                    of.write("Timestamp  : {0}\r\n".format(row[1]))
                                 of.write("\r\n")
                             
                     except sqlite3.Error as e:
-                        logging.error("{}".format(e.args[0]))
-                        print("[ERROR] {}".format(e.args[0]))
+                        logging.error("{0}".format(e.args[0]))
+                        print("[ERROR] {0}".format(e.args[0]))
                     finally:
                         if conn:
                             conn.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
             
             elif self._os_version in ["lion", "snow_leopard"]:
                 query = "SELECT request_key, time_stamp FROM cfurl_cache_response"
                 if os.path.isfile(file):
-                    of.write("Source File: {}\r\n\r\n".format(file))
+                    of.write("Source File: {0}\r\n\r\n".format(file))
                     conn = None
                     try:
                         conn = sqlite3.connect(file)
@@ -103,23 +103,23 @@ class UsersSafariCache(Plugin):
                                 if row[0] is None:
                                     of.write("Request Key:\r\n")
                                 else:
-                                    of.write("Request Key: {}\r\n".format(row[0]))
+                                    of.write("Request Key: {0}\r\n".format(row[0]))
                                 if row[1] is None:
                                     of.write("Timestamp  :\r\n")
                                 else:
-                                    of.write("Timestamp  : {}\r\n".format(row[1]))
+                                    of.write("Timestamp  : {0}\r\n".format(row[1]))
                                 of.write("\r\n")
 
                     except sqlite3.Error as e:
-                        logging.error("{}".format(e.args[0]))
-                        print("[ERROR] {}".format(e.args[0]))
+                        logging.error("{0}".format(e.args[0]))
+                        print("[ERROR] {0}".format(e.args[0]))
                     finally:
                         if conn:
                             conn.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
