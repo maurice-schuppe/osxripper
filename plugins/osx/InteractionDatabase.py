f14c95d5c3621c5acfbc6bdeb48e1e8b4c09b8b6
bolodev <bolodev@users.noreply.github.com>
2016-05-22 00:11:27 +0100

    Code refactoring to use osxripper_time

--- plugins/osx/InteractionDatabase.py
+++ plugins/osx/InteractionDatabase.py
@@ -2,6 +2,7 @@ from riplib.Plugin import Plugin
 import codecs
 import logging
 import os
+import osxripper_time
 import sqlite3
 
 __author__ = 'osxripper'
@@ -33,19 +34,25 @@ class InteractionDatabase(Plugin):
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             database_file = os.path.join(self._input_dir, "private", "var", "db", "CoreDuet", "People", self._data_file)
             if self._os_version == "el_capitan":
-                if os.path.isfile(database_file):
+                if not os.path.isfile(database_file):
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(self._data_file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(self._data_file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(self._data_file))
+                else:
                     of.write("Source Database: {0}\r\n\r\n".format(database_file))
                     conn = None
                     try:
                         conn = sqlite3.connect(database_file)
+                        conn.row_factory = sqlite3.Row
+                        # cocoa time 2001/1/1 00:00
                         query = "SELECT zpk.z_name,zc.zdisplayname,zc.zidentifier," \
-                                "datetime(zc.zcreationdate + 978307200, 'unixepoch')," \
-                                "datetime(zc.zfirstincomingrecipientdate + 978307200, 'unixepoch')," \
-                                "datetime(zc.zfirstincomingsenderdate + 978307200, 'unixepoch')," \
-                                "datetime(zc.zfirstoutgoingrecipientdate + 978307200, 'unixepoch')," \
-                                "datetime(zc.zlastincomingrecipientdate + 978307200, 'unixepoch')," \
-                                "datetime(zc.zlastincomingsenderdate + 978307200, 'unixepoch')," \
-                                "datetime(zc.zlastoutgoingrecipientdate + 978307200, 'unixepoch')" \
+                                "zc.zcreationdate," \
+                                "zc.zfirstincomingrecipientdate," \
+                                "zc.zfirstincomingsenderdate," \
+                                "zc.zfirstoutgoingrecipientdate," \
+                                "zc.zlastincomingrecipientdate," \
+                                "zc.zlastincomingsenderdate," \
+                                "zc.zlastoutgoingrecipientdate" \
                                 " FROM z_primarykey zpk,zcontacts zc WHERE zpk.z_ent = zc.z_ent"
                         with conn:
                             cur = conn.cursor()
@@ -53,49 +60,40 @@ class InteractionDatabase(Plugin):
                             rows = cur.fetchall()
                             if len(rows) > 0:
                                 for row in rows:
-                                    if row[0] is None:
-                                        of.write("Name                         :\r\n")
-                                    else:
-                                        of.write("Name                         : {0}\r\n".format(row[0]))
-                                    if row[1] is None:
-                                        of.write("Display Name                 :\r\n")
-                                    else:
-                                        of.write("Display Name                 : {0}\r\n".format(row[1]))
-                                    if row[2] is None:
-                                        of.write("Identifier                   :\r\n")
-                                    else:
-                                        of.write("Identifier                   : {0}\r\n".format(row[2]))
-                                    if row[3] is None:
-                                        of.write("Creation Date                :\r\n")
-                                    else:
-                                        of.write("Creation Date                : {0}\r\n".format(row[3]))
-                                    if row[4] is None:
-                                        of.write("First Incoming Recipient Date:\r\n")
-                                    else:
-                                        of.write("First Incoming Recipient Date: {0}\r\n".format(row[4]))
-                                    if row[5] is None:
-                                        of.write("First Incoming Sender Date   :\r\n")
-                                    else:
-                                        of.write("First Incoming Sender Date   : {0}\r\n".format(row[5]))
-                                    if row[6] is None:
-                                        of.write("First Outgoing Recipient Date:\r\n")
-                                    else:
-                                        of.write("First Outgoing Recipient Date: {0}\r\n".format(row[6]))
-                                    if row[7] is None:
-                                        of.write("Last Incoming Recipient Date :\r\n")
-                                    else:
-                                        of.write("Last Incoming Recipient Date : {0}\r\n".format(row[7]))
-                                    if row[8] is None:
-                                        of.write("Last Incoming Sender Date    :\r\n")
-                                    else:
-                                        of.write("Last Incoming Sender Date    : {0}\r\n".format(row[8]))
-                                    if row[9] is None:
-                                        of.write("Last Outgoing Recipient Date :\r\n")
-                                    else:
-                                        of.write("Last Outgoing Recipient Date : {0}\r\n".format(row[9]))
+                                    creationdate = \
+                                        osxripper_time.get_cocoa_seconds(row["zcreationdate"])
+                                    firstincomingrecipientdate = \
+                                        osxripper_time.get_cocoa_seconds(row["zfirstincomingrecipientdate"])
+                                    firstincomingsenderdate = \
+                                        osxripper_time.get_cocoa_seconds(row["zfirstincomingsenderdate"])
+                                    firstoutgoingrecipientdate = \
+                                        osxripper_time.get_cocoa_seconds(row["zfirstoutgoingrecipientdate"])
+                                    lastincomingrecipientdate = \
+                                        osxripper_time.get_cocoa_seconds(row["zlastincomingrecipientdate"])
+                                    lastincomingsenderdate = \
+                                        osxripper_time.get_cocoa_seconds(row["zlastincomingsenderdate"])
+                                    lastoutgoingrecipientdate = \
+                                        osxripper_time.get_cocoa_seconds(row["zlastincomingsenderdate"])
+
+                                    of.write("Name                         : {0}\r\n".format(row["z_name"]))
+                                    of.write("Display Name                 : {0}\r\n".format(row["zdisplayname"]))
+                                    of.write("Identifier                   : {0}\r\n".format(row["zidentifier"]))
+                                    of.write("Creation Date                : {0}\r\n".format(creationdate))
+                                    of.write("First Incoming Recipient Date: {0}\r\n"
+                                             .format(firstincomingrecipientdate))
+                                    of.write("First Incoming Sender Date   : {0}\r\n"
+                                             .format(firstincomingsenderdate))
+                                    of.write("First Outgoing Recipient Date: {0}\r\n"
+                                             .format(firstoutgoingrecipientdate))
+                                    of.write("Last Incoming Recipient Date : {0}\r\n"
+                                             .format(lastincomingrecipientdate))
+                                    of.write("Last Incoming Sender Date    : {0}\r\n"
+                                             .format(lastincomingsenderdate))
+                                    of.write("Last Outgoing Recipient Date : {0}\r\n"
+                                             .format(lastoutgoingrecipientdate))
                                     of.write("\r\n")
                             else:
-                                of.write("No data in dtabase.\r\n")
+                                of.write("No data in database.\r\n")
                         of.write("\r\n")
                     except sqlite3.Error as e:
                         logging.error("{0}".format(e.args[0]))
@@ -104,10 +102,7 @@ class InteractionDatabase(Plugin):
                         if conn:
                             conn.close()
                     of.write("="*50 + "\r\n")
-                else:
-                    logging.warning("File: index.sqlite does not exist or cannot be found.\r\n")
-                    of.write("[WARNING] File: index.sqlite does not exist or cannot be found.\r\n")
-                    print("[WARNING] File: index.sqlite does not exist or cannot be found.")
+
             elif self._os_version in ["yosemite", "mavericks", "mountain_lion", "lion", "snow_leopard"]:
                 logging.info("This version of OSX is not supported this plugin.")
                 print("[INFO] This version of OSX is not supported this plugin.")
@@ -115,5 +110,4 @@ class InteractionDatabase(Plugin):
             else:
                 logging.warning("Not a known OSX version.")
                 print("[WARNING] Not a known OSX version.")
-            # of.write("="*40 + "\r\n\r\n")
         of.close()
