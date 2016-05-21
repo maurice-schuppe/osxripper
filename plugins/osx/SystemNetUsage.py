38062612bd1da6b81f4ea8b494de66013060af28
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:52:52 +0100

    Code clean - positional parameters in string format

--- plugins/osx/SystemNetUsage.py
+++ plugins/osx/SystemNetUsage.py
@@ -32,7 +32,7 @@ class SystemNetUsage(Plugin):
         with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             file = os.path.join(self._input_dir, "private", "var", "networkd", self._data_file)
-            of.write("Source File: {}\r\n\r\n".format(file))
+            of.write("Source File: {0}\r\n\r\n".format(file))
             if self._os_version == "el_capitan":
                 if os.path.isfile(file):
                     pass
@@ -47,15 +47,15 @@ class SystemNetUsage(Plugin):
                         run_live_usage_query(conn, of)
                         of.write("\r\n")
                     except sqlite3.Error as e:
-                        logging.error("{}".format(e.args[0]))
-                        print("[ERROR] {}".format(e.args[0]))
+                        logging.error("{0}".format(e.args[0]))
+                        print("[ERROR] {0}".format(e.args[0]))
                     finally:
                         if conn:
                             conn.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
 
             elif self._os_version in ["yosemite", "mavericks", "mountain_lion", "lion", "snow_leopard"]:
                 logging.info("This version of OSX is not supported by this plugin.")
@@ -80,19 +80,19 @@ def run_process_query(sqlite_connection, output_file):
             if row[0] is None:
                 output_file.write("Name           :\r\n")
             else:
-                output_file.write("Name           : {}\r\n".format(row[0]))
+                output_file.write("Name           : {0}\r\n".format(row[0]))
             if row[1] is None:
                 output_file.write("Process        :\r\n")
             else:
-                output_file.write("Process        : {}\r\n".format(row[1]))
+                output_file.write("Process        : {0}\r\n".format(row[1]))
             if row[2] is None:
                 output_file.write("First Timestamp:\r\n")
             else:
-                output_file.write("First Timestamp: {}\r\n".format(row[2]))
+                output_file.write("First Timestamp: {0}\r\n".format(row[2]))
             if row[3] is None:
                 output_file.write("Timestamp      :\r\n")
             else:
-                output_file.write("Timestamp      : {}\r\n".format(row[3]))
+                output_file.write("Timestamp      : {0}\r\n".format(row[3]))
             output_file.write("\r\n")
 
 
@@ -108,39 +108,39 @@ def run_live_usage_query(sqlite_connection, output_file):
             if row[0] is None:
                 output_file.write("Name     :\r\n")
             else:
-                output_file.write("Name     : {}\r\n".format(row[0]))
+                output_file.write("Name     : {0}\r\n".format(row[0]))
             if row[1] is None:
                 output_file.write("Process  :\r\n")
             else:
-                output_file.write("Process  : {}\r\n".format(row[1]))
+                output_file.write("Process  : {0}\r\n".format(row[1]))
             if row[2] is None:
                 output_file.write("Timestamp:\r\n")
             else:
-                output_file.write("Timestamp: {}\r\n".format(row[2]))
+                output_file.write("Timestamp: {0}\r\n".format(row[2]))
             if row[3] is None:
                 output_file.write("WiFi In  :\r\n")
             else:
-                output_file.write("WiFi In  : {}\r\n".format(row[3]))
+                output_file.write("WiFi In  : {0}\r\n".format(row[3]))
             if row[4] is None:
                 output_file.write("WiFi Out :\r\n")
             else:
-                output_file.write("WiFi Out : {}\r\n".format(row[4]))
+                output_file.write("WiFi Out : {0}\r\n".format(row[4]))
             if row[5] is None:
                 output_file.write("Wired In :\r\n")
             else:
-                output_file.write("Wired In : {}\r\n".format(row[5]))
+                output_file.write("Wired In : {0}\r\n".format(row[5]))
             if row[6] is None:
                 output_file.write("Wired Out:\r\n")
             else:
-                output_file.write("Wired Out: {}\r\n".format(row[6]))
+                output_file.write("Wired Out: {0}\r\n".format(row[6]))
             if row[7] is None:
                 output_file.write("WAN In   :\r\n")
             else:
-                output_file.write("WAN In   : {}\r\n".format(row[7]))
+                output_file.write("WAN In   : {0}\r\n".format(row[7]))
             if row[8] is None:
                 output_file.write("WAN Out  :\r\n")
             else:
-                output_file.write("WAN Out  : {}\r\n".format(row[8]))
+                output_file.write("WAN Out  : {0}\r\n".format(row[8]))
             output_file.write("\r\n")
 
 
@@ -156,7 +156,7 @@ def run_network_attachment_query(sqlite_connection, output_file):
                 if row[0] is None:
                     output_file.write("Name           :\r\n")
                 else:
-                    output_file.write("Name           : {}\r\n".format(row[0]))
+                    output_file.write("Name           : {0}\r\n".format(row[0]))
                 if row[1] is None:
                     output_file.write("Network        :\r\n")
                     output_file.write("MAC Address    :\r\n")
@@ -165,14 +165,14 @@ def run_network_attachment_query(sqlite_connection, output_file):
                     dash_index = ident.rfind("-")
                     network_name = ident[0:dash_index]
                     network_mac = ident[dash_index+1:len(ident)]
-                    output_file.write("Network        : {}\r\n".format(network_name))
-                    output_file.write("MAC Address    : {}\r\n".format(network_mac))
+                    output_file.write("Network        : {0}\r\n".format(network_name))
+                    output_file.write("MAC Address    : {0}\r\n".format(network_mac))
                 if row[2] is None:
                     output_file.write("First Timestamp:\r\n")
                 else:
-                    output_file.write("First Timestamp: {}\r\n".format(row[2]))
+                    output_file.write("First Timestamp: {0}\r\n".format(row[2]))
                 if row[3] is None:
                     output_file.write("Timestamp      :\r\n")
                 else:
-                    output_file.write("Timestamp      : {}\r\n".format(row[3]))
+                    output_file.write("Timestamp      : {0}\r\n".format(row[3]))
                 output_file.write("\r\n")
