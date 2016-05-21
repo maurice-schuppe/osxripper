67acf2a709b7e1aa781d89eb42c2cc6f65bdd130
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:48:52 +0100

    Code clean - positional parameters in string format

--- plugins/osx/SystemAuthDB.py
+++ plugins/osx/SystemAuthDB.py
@@ -35,7 +35,7 @@ class SystemAuthDB(Plugin):
                     "datetime(created + 978307200, 'unixepoch'), datetime(modified + 978307200, 'unixepoch'), " \
                     "identifier, comment FROM rules ORDER BY name"
             file = os.path.join(self._input_dir, "private", "var", "db", self._data_file)
-            of.write("Source File: {}\r\n\r\n".format(file))
+            of.write("Source File: {0}\r\n\r\n".format(file))
             if self._os_version in ["el_capitan", "yosemite", "mavericks"]:
                 if os.path.isfile(file):
                     conn = None
@@ -49,58 +49,58 @@ class SystemAuthDB(Plugin):
                                 if row[0] is None:
                                     of.write("Name      :\r\n")
                                 else:
-                                    of.write("Name      : {}\r\n".format(row[0]))
+                                    of.write("Name      : {0}\r\n".format(row[0]))
                                 if row[1] is None:
                                     of.write("Group     :\r\n")
                                 else:
-                                    of.write("Group     : {}\r\n".format(row[1]))
+                                    of.write("Group     : {0}\r\n".format(row[1]))
                                 if row[2] is None:
                                     of.write("Type      :\r\n")
                                 else:
-                                    of.write("Type      : {}\r\n".format(row[2]))
+                                    of.write("Type      : {0}\r\n".format(row[2]))
                                 if row[3] is None:
                                     of.write("Class     :\r\n")
                                 else:
-                                    of.write("Class     : {}\r\n".format(row[3]))
+                                    of.write("Class     : {0}\r\n".format(row[3]))
                                 if row[4] is None:
                                     of.write("Tries     :\r\n")
                                 else:
-                                    of.write("Tries     : {}\r\n".format(row[4]))
+                                    of.write("Tries     : {0}\r\n".format(row[4]))
                                 if row[5] is None:
                                     of.write("Version   :\r\n")
                                 else:
-                                    of.write("Version   : {}\r\n".format(row[5]))
+                                    of.write("Version   : {0}\r\n".format(row[5]))
                                 if row[6] is None:
                                     of.write("K-OF-N    :\r\n")
                                 else:
-                                    of.write("K-OF-N    : {}\r\n".format(row[6]))
+                                    of.write("K-OF-N    : {0}\r\n".format(row[6]))
                                 if row[7] is None:
                                     of.write("Created   :\r\n")
                                 else:
-                                    of.write("Created   : {}\r\n".format(row[7]))
+                                    of.write("Created   : {0}\r\n".format(row[7]))
                                 if row[8] is None:
                                     of.write("Modified  :\r\n")
                                 else:
-                                    of.write("Modified  : {}\r\n".format(row[8]))
+                                    of.write("Modified  : {0}\r\n".format(row[8]))
                                 if row[9] is None:
                                     of.write("Identifier:\r\n")
                                 else:
-                                    of.write("Identifier: {}\r\n".format(row[9]))
+                                    of.write("Identifier: {0}\r\n".format(row[9]))
                                 if row[10] is None:
                                     of.write("Comment   :\r\n")
                                 else:
-                                    of.write("Comment   : {}\r\n".format(row[10]))
+                                    of.write("Comment   : {0}\r\n".format(row[10]))
                                 of.write("\r\n")
                     except sqlite3.Error as e:
-                        logging.error("{}".format(e.args[0]))
-                        print("[ERROR] {}".format(e.args[0]))
+                        logging.error("{0}".format(e.args[0]))
+                        print("[ERROR] {0}".format(e.args[0]))
                     finally:
                         if conn:
                             conn.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
 
             elif self._os_version in ["mountain_lion", "lion", "snow_leopard"]:
                 logging.info("This version of OSX is not supported by this plugin.")
