5f6e73c1f8dae0d8726cf362f7baff4a6057df56
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:57:24 +0100

    Code clean - positional parameters in string format

--- plugins/osx/UserAccountsPlist.py
+++ plugins/osx/UserAccountsPlist.py
@@ -52,28 +52,28 @@ class UserAccountsPlist(Plugin):
                         name = None
                         if "home" in pl and "/Users" in pl["home"][0]:  # Only /Users
                             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-                            of.write("Source File: {}\r\n\r\n".format(file))
+                            of.write("Source File: {0}\r\n\r\n".format(file))
                             if "name" in pl:
-                                of.write("Name          : {}\r\n".format(pl["name"][0]))
+                                of.write("Name          : {0}\r\n".format(pl["name"][0]))
                                 name = pl["name"][0]
                             if "realname" in pl:
-                                of.write("Real Name     : {}\r\n".format(pl["realname"][0]))
+                                of.write("Real Name     : {0}\r\n".format(pl["realname"][0]))
                             if "home" in pl:
-                                of.write("Home          : {}\r\n".format(pl["home"][0]))
+                                of.write("Home          : {0}\r\n".format(pl["home"][0]))
                             if "hint" in pl:
-                                of.write("Password Hint : {}\r\n".format(pl["hint"][0]))
+                                of.write("Password Hint : {0}\r\n".format(pl["hint"][0]))
                             if "authentication_authority" in pl:
-                                of.write("Authentication: {}\r\n".format(pl["authentication_authority"]))
+                                of.write("Authentication: {0}\r\n".format(pl["authentication_authority"]))
                             if "uid" in pl:
-                                of.write("UID           : {}\r\n".format(pl["uid"][0]))
+                                of.write("UID           : {0}\r\n".format(pl["uid"][0]))
                             if "gid" in pl:
-                                of.write("GID           : {}\r\n".format(pl["gid"][0]))
+                                of.write("GID           : {0}\r\n".format(pl["gid"][0]))
                             if "generateduid" in pl:
-                                of.write("Generated UID : {}\r\n".format(pl["generateduid"][0]))
+                                of.write("Generated UID : {0}\r\n".format(pl["generateduid"][0]))
                             if "shell" in pl:
-                                of.write("Shell         : {}\r\n".format(pl["shell"][0]))
+                                of.write("Shell         : {0}\r\n".format(pl["shell"][0]))
                             if "picture" in pl:
-                                of.write("Picture       : {}\r\n".format(pl["picture"][0]))
+                                of.write("Picture       : {0}\r\n".format(pl["picture"][0]))
                             if "jpegphoto" in pl and name is not None:
                                 jpeg = os.path.join(self._output_dir, "UserAccounts-" + name + "-jpgphoto.jpg")
                                 with open(jpeg, "wb") as jof:
@@ -85,9 +85,9 @@ class UserAccountsPlist(Plugin):
                     except KeyError:
                         pass
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.".format(file))
             elif self._os_version == "snow_leopard":
                 # Snow Leopard uses plain plists
                 with open(file, 'rb') as pl:
@@ -95,31 +95,30 @@ class UserAccountsPlist(Plugin):
                         name = None
                         plist = plistlib.load(pl)
                         pl.close()
-                        # print(plist)
                         if "home" in plist and "/Users" in plist["home"][0]:  # Only /Users
                             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-                            of.write("Source File: {}\r\n\r\n".format(file))
+                            of.write("Source File: {0}\r\n\r\n".format(file))
                             if "name" in plist:
-                                of.write("Name          : {}\r\n".format(plist["name"][0]))
+                                of.write("Name          : {0}\r\n".format(plist["name"][0]))
                                 name = plist["name"][0]
                             if "realname" in plist:
-                                of.write("Real Name     : {}\r\n".format(plist["realname"][0]))
+                                of.write("Real Name     : {0}\r\n".format(plist["realname"][0]))
                             if "home" in plist:
-                                of.write("Home          : {}\r\n".format(plist["home"][0]))
+                                of.write("Home          : {0}\r\n".format(plist["home"][0]))
                             if "hint" in plist:
-                                of.write("Password Hint : {}\r\n".format(plist["hint"][0]))
+                                of.write("Password Hint : {0}\r\n".format(plist["hint"][0]))
                             if "authentication_authority" in plist:
-                                of.write("Authentication: {}\r\n".format(plist["authentication_authority"]))
+                                of.write("Authentication: {0}\r\n".format(plist["authentication_authority"]))
                             if "uid" in plist:
-                                of.write("UID           : {}\r\n".format(plist["uid"][0]))
+                                of.write("UID           : {0}\r\n".format(plist["uid"][0]))
                             if "gid" in plist:
-                                of.write("GID           : {}\r\n".format(plist["gid"][0]))
+                                of.write("GID           : {0}\r\n".format(plist["gid"][0]))
                             if "generateduid" in plist:
-                                of.write("Generated UID : {}\r\n".format(plist["generateduid"][0]))
+                                of.write("Generated UID : {0}\r\n".format(plist["generateduid"][0]))
                             if "shell" in plist:
-                                of.write("Shell         : {}\r\n".format(plist["shell"][0]))
+                                of.write("Shell         : {0}\r\n".format(plist["shell"][0]))
                             if "picture" in plist:
-                                of.write("Picture       : {}\r\n".format(plist["picture"][0]))
+                                of.write("Picture       : {0}\r\n".format(plist["picture"][0]))
                             if "jpegphoto" in plist and name is not None:
                                 jpeg = os.path.join(self._output_dir, "UserAccounts-" + name + "-jpgphoto.jpg")
                                 with open(jpeg, "wb") as jof:
