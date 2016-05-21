621984d5c5e36289b272783acacc45dea331d9ad
bolodev <bolodev@users.noreply.github.com>
2016-05-21 21:13:52 +0100

    Code clean - positional parameters in string format

--- plugins/osx/UsersLoginWindowPlist.py
+++ plugins/osx/UsersLoginWindowPlist.py
@@ -38,11 +38,11 @@ class UsersLoginWindowPlist(Plugin):
                     if os.path.isfile(plist):
                         self.__parse_bplist(plist, username)
                     else:
-                        logging.warning("{} does not exist.".format(plist))
-                        print("[WARNING] {} does not exist.".format(plist))
+                        logging.warning("{0} does not exist.".format(plist))
+                        print("[WARNING] {0} does not exist.".format(plist))
         else:
-            logging.warning("{} does not exist.".format(users_path))
-            print("[WARNING] {} does not exist.".format(users_path))
+            logging.warning("{0} does not exist.".format(users_path))
+            print("[WARNING] {0} does not exist.".format(users_path))
             
     def __parse_bplist(self, file, username):
         """
@@ -50,57 +50,57 @@ class UsersLoginWindowPlist(Plugin):
         """
         with codecs.open(os.path.join(self._output_dir, "Users_" + username + ".txt"), "a", encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
-            of.write("Source File: {}\r\n\r\n".format(file))
+            of.write("Source File: {0}\r\n\r\n".format(file))
             if self._os_version == "el_capitan":
                 if os.path.isfile(file):
                     bplist = open(file, "rb")
                     pl = ccl_bplist.load(bplist)
                     try:
                         if "TALLogoutReason" in pl:
-                            of.write("Logout Reason     : {}\r\n".format(pl["TALLogoutReason"]))
+                            of.write("Logout Reason     : {0}\r\n".format(pl["TALLogoutReason"]))
                     except KeyError:
                         pass
                     bplist.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
             elif self._os_version in ["yosemite", "mavericks", "mountain_lion"]:
                 if os.path.isfile(file):
                     bplist = open(file, "rb")
                     pl = ccl_bplist.load(bplist)
                     try:
                         if "TALLogoutReason" in pl:
-                            of.write("Logout Reason     : {}\r\n".format(pl["TALLogoutReason"]))
+                            of.write("Logout Reason     : {0}\r\n".format(pl["TALLogoutReason"]))
                         if "TALLogoutSavesState" in pl:
-                            of.write("Save State        : {}\r\n".format(pl["TALLogoutSavesState"]))
+                            of.write("Save State        : {0}\r\n".format(pl["TALLogoutSavesState"]))
                     except KeyError:
                         pass
                     bplist.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
             elif self._os_version == "lion":
                 if os.path.isfile(file):
                     bplist = open(file, "rb")
                     pl = ccl_bplist.load(bplist)
                     try:
                         if "TALLogoutReason" in pl:
-                            of.write("Logout Reason        : {}\r\n".format(pl["TALLogoutReason"]))
+                            of.write("Logout Reason        : {0}\r\n".format(pl["TALLogoutReason"]))
                         if "AutoOpenedWindowDictionary" in pl:
                             auto_open = pl["AutoOpenedWindowDictionary"]
                             if "CurrentSpaceID" in auto_open:
-                                of.write("Current Space ID     : {}\r\n".format(auto_open["CurrentSpaceID"]))
+                                of.write("Current Space ID     : {0}\r\n".format(auto_open["CurrentSpaceID"]))
                             if "NumberOfSpaces" in auto_open:
-                                of.write("Number Of Spaces     : {}\r\n".format(auto_open["NumberOfSpaces"]))
+                                of.write("Number Of Spaces     : {0}\r\n".format(auto_open["NumberOfSpaces"]))
                     except KeyError:
                         pass
                     bplist.close()
                 else:
-                    logging.warning("File: {} does not exist or cannot be found.\r\n".format(file))
-                    of.write("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
-                    print("[WARNING] File: {} does not exist or cannot be found.\r\n".format(file))
+                    logging.warning("File: {0} does not exist or cannot be found.\r\n".format(file))
+                    of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
+                    print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
             elif self._os_version == "snow_leopard":
                 logging.info("This version of OSX is not supported by this plugin.")
                 print("[INFO] This version of OSX is not supported by this plugin.")
