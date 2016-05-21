8c51f59eeafdda01188c7415d1727533d2a009c9
bolodev <bolodev@users.noreply.github.com>
2016-05-21 20:45:39 +0100

    Code clean - positional parameters in string format

--- plugins/osx/Playlists.py
+++ plugins/osx/Playlists.py
@@ -32,21 +32,21 @@ class Playlists(Plugin):
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             if self._os_version in ["el_capitan", "yosemite", "mavericks", "mountain_lion", "lion"]:
                 working_dir = os.path.join(self._input_dir, "private", "var", "db", "BootCaches")
-                of.write("Source Directory: {}\r\n\r\n".format(working_dir))
+                of.write("Source Directory: {0}\r\n\r\n".format(working_dir))
                 if os.path.isdir(working_dir):
                     file_listing = os.listdir(working_dir)
                     for f in file_listing:
                         test_file = os.path.join(working_dir, f)
                         if os.path.isdir(test_file):
-                            of.write("Generated User ID: {}\r\n".format(f))
+                            of.write("Generated User ID: {0}\r\n".format(f))
                             user_playlists = os.listdir(test_file)
                             for user_file in user_playlists:
-                                of.write("\t{}\r\n".format(user_file))
+                                of.write("\t{0}\r\n".format(user_file))
                             of.write("\r\n")
                 else:
-                    logging.warning("Directory: {} does not exist or cannot be found.\r\n".format(working_dir))
-                    of.write("[WARNING] Directory: {} does not exist or cannot be found.\r\n".format(working_dir))
-                    print("[WARNING] Directory: {} does not exist or cannot be found.\r\n".format(working_dir))
+                    logging.warning("Directory: {0} does not exist or cannot be found.\r\n".format(working_dir))
+                    of.write("[WARNING] Directory: {0} does not exist or cannot be found.\r\n".format(working_dir))
+                    print("[WARNING] Directory: {0} does not exist or cannot be found.\r\n".format(working_dir))
             elif self._os_version == "snow_leopard":
                 logging.info("This version of OSX is not supported by this plugin.")
                 print("[INFO] This version of OSX is not supported by this plugin.")
