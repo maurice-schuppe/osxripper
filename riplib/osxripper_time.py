6f5de9c7ad5f828932a319729555062d1214b574
bolodev <bolodev@users.noreply.github.com>
2016-06-01 23:56:50 +0100

    Added get_gregorian_* functions

--- riplib/osxripper_time.py
+++ riplib/osxripper_time.py
@@ -3,6 +3,46 @@ import datetime
 
 COCOA_EPOCH = datetime.datetime(2001, 1, 1)
 UNIX_EPOCH = datetime.datetime(1970, 1, 1)
+GREGORIAN_1601 = datetime.datetime(1601, 1, 1)
+
+
+def get_gregorian_seconds(delta_date):
+    """
+    Get the date time with a second delta
+    """
+    if delta_date is None:
+        return "[ERROR] Not a date value: None"
+    else:
+        try:
+            return GREGORIAN_1601 + datetime.timedelta(seconds=delta_date)
+        except OverflowError:
+            "[ERROR] unknown date value: {0}".format(delta_date)
+
+
+def get_gregorian_micros(delta_date):
+    """
+    Get the date time with a microsecond delta
+    """
+    if delta_date is None:
+        return "[ERROR] Not a date value: None"
+    else:
+        try:
+            return GREGORIAN_1601 + datetime.timedelta(microseconds=delta_date)
+        except OverflowError:
+            "[ERROR] unknown date value: {0}".format(delta_date)
+
+
+def get_gregorian_millis(delta_date):
+    """
+    Get the date time with a millisecond delta
+    """
+    if delta_date is None:
+        return "[ERROR] Not a date value: None"
+    else:
+        try:
+            return GREGORIAN_1601 + datetime.timedelta(milliseconds=delta_date)
+        except OverflowError:
+            "[ERROR] unknown date value: {0}".format(delta_date)
 
 
 def get_unix_seconds(delta_date):
@@ -29,6 +69,7 @@ def get_unix_micros(delta_date):
             return UNIX_EPOCH + datetime.timedelta(microseconds=delta_date)
         except OverflowError:
             "[ERROR] unknown date value: {0}".format(delta_date)
+
 
 def get_unix_millis(delta_date):
     """
