ee120e1131a46cdb026a86000bad4cb0a521a030
bolodev <bolodev@users.noreply.github.com>
2016-06-20 23:47:52 +0100

    Clean up of SQL

--- plugins/osx/UsersQuarantineEventsV2.py
+++ plugins/osx/UsersQuarantineEventsV2.py
@@ -3,6 +3,7 @@ import codecs
 import datetime
 import logging
 import os
+import osxripper_time
 import sqlite3
 
 __author__ = 'osxripper'
@@ -59,62 +60,34 @@ class UsersQuarantineEventsV2(Plugin):
                          encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             if self._os_version in ["el_capitan", "yosemite", "mavericks", "mountain_lion", "lion"]:
-                query = "SELECT * FROM LSQuarantineEvent"
+                query = "SELECT LSQuarantineEventIdentifier,LSQuarantineTimeStamp,LSQuarantineAgentBundleIdentifier," \
+                        "LSQuarantineAgentName,LSQuarantineDataURLString,LSQuarantineSenderName," \
+                        "LSQuarantineSenderAddress,LSQuarantineTypeNumber,LSQuarantineOriginTitle," \
+                        "LSQuarantineOriginURLString,LSQuarantineOriginAlias FROM LSQuarantineEvent"
                 if os.path.isfile(file):
                     of.write("Source File: {0}\r\n\r\n".format(file))
                     conn = None
                     try:
                         conn = sqlite3.connect(file)
+                        conn.row_factory = sqlite3.Row
                         with conn:    
                             cur = conn.cursor()
                             cur.execute(query)
                             rows = cur.fetchall()
                             for row in rows:
-                                if row[0] is None:
-                                    of.write("Event Identifier      :\r\n")
-                                else:
-                                    of.write("Event Identifier      : {0}\r\n".format(row[0]))
-                                if row[1] is None:
-                                    of.write("Timestamp             :\r\n")
-                                else:
-                                    of.write("Timestamp             : {0}\r\n"
-                                             .format(datetime.datetime.fromtimestamp(row[1] + 978307200)))
-                                if row[2] is None:
-                                    of.write("AgentBundle Identifier:\r\n")
-                                else:
-                                    of.write("AgentBundle Identifier: {0}\r\n".format(row[2]))
-                                if row[3] is None:
-                                    of.write("Agent Name            :\r\n")
-                                else:
-                                    of.write("Agent Name            : {0}\r\n".format(row[3]))
-                                if row[4] is None:
-                                    of.write("Data URL String       :\r\n")
-                                else:                            
-                                    of.write("Data URL String       : {0}\r\n".format(row[4]))
-                                if row[5] is None:
-                                    of.write("Sender Name           :\r\n")
-                                else:
-                                    of.write("Sender Name           : {0}\r\n".format(row[5]))
-                                if row[6] is None:
-                                    of.write("Sender Address        :\r\n")
-                                else:
-                                    of.write("Sender Address        : {0}\r\n".format(row[6]))
-                                if row[7] is None:
-                                    of.write("Type Number           :\r\n")
-                                else:
-                                    of.write("Type Number           : {0}\r\n".format(row[7]))
-                                if row[8] is None:
-                                    of.write("Origin Title          :\r\n")
-                                else:
-                                    of.write("Origin Title          : {0}\r\n".format(row[8]))
-                                if row[9] is None:
-                                    of.write("Origin URL String     : \r\n")
-                                else:
-                                    of.write("Origin URL String     : {0}\r\n".format(row[9]))
-                                if row[10] is None:
-                                    of.write("Origin Alias          :\r\n")
-                                else:    
-                                    of.write("Origin Alias          : {0}\r\n".format(row[10]))
+                                timestamp = osxripper_time.get_cocoa_seconds(row["LSQuarantineTimeStamp"])
+                                of.write("Event Identifier      : {0}\r\n".format(row["LSQuarantineEventIdentifier"]))
+                                of.write("Timestamp             : {0}\r\n".format(timestamp))
+                                of.write("AgentBundle Identifier: {0}\r\n"
+                                         .format(row["LSQuarantineAgentBundleIdentifier"]))
+                                of.write("Agent Name            : {0}\r\n".format(row["LSQuarantineAgentName"]))
+                                of.write("Data URL String       : {0}\r\n".format(row["LSQuarantineDataURLString"]))
+                                of.write("Sender Name           : {0}\r\n".format(row["LSQuarantineSenderName"]))
+                                of.write("Sender Address        : {0}\r\n".format(row["LSQuarantineSenderAddress"]))
+                                of.write("Type Number           : {0}\r\n".format(row["LSQuarantineTypeNumber"]))
+                                of.write("Origin Title          : {0}\r\n".format(row["LSQuarantineOriginTitle"]))
+                                of.write("Origin URL String     : {0}\r\n".format(row["LSQuarantineOriginURLString"]))
+                                of.write("Origin Alias          : {0}\r\n".format(row["LSQuarantineOriginAlias"]))
                                 of.write("\r\n")
                             
                     except sqlite3.Error as e:
@@ -128,62 +101,34 @@ class UsersQuarantineEventsV2(Plugin):
                     of.write("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
                     print("[WARNING] File: {0} does not exist or cannot be found.\r\n".format(file))
             elif self._os_version == "snow_leopard":
-                query = "SELECT * FROM LSQuarantineEvent"
+                query = "SELECT LSQuarantineEventIdentifier,LSQuarantineTimeStamp,LSQuarantineAgentBundleIdentifier," \
+                        "LSQuarantineAgentName,LSQuarantineDataURLString,LSQuarantineSenderName," \
+                        "LSQuarantineSenderAddress,LSQuarantineTypeNumber,LSQuarantineOriginTitle," \
+                        "LSQuarantineOriginURLString,LSQuarantineOriginAlias FROM LSQuarantineEvent"
                 if os.path.isfile(file):
                     of.write("Source File: {0}\r\n\r\n".format(file))
                     conn = None
                     try:
                         conn = sqlite3.connect(file)
+                        conn.row_factory = sqlite3.Row
                         with conn:
                             cur = conn.cursor()
                             cur.execute(query)
                             rows = cur.fetchall()
                             for row in rows:
-                                if row[0] is None:
-                                    of.write("Event Identifier      :\r\n")
-                                else:
-                                    of.write("Event Identifier      : {0}\r\n".format(row[0]))
-                                if row[1] is None:
-                                    of.write("Timestamp             :\r\n")
-                                else:
-                                    of.write("Timestamp             : {0}\r\n"
-                                             .format(datetime.datetime.fromtimestamp(row[1] + 978307200)))
-                                if row[2] is None:
-                                    of.write("AgentBundle Identifier:\r\n")
-                                else:
-                                    of.write("AgentBundle Identifier: {0}\r\n".format(row[2]))
-                                if row[3] is None:
-                                    of.write("Agent Name            :\r\n")
-                                else:
-                                    of.write("Agent Name            : {0}\r\n".format(row[3]))
-                                if row[4] is None:
-                                    of.write("Data URL String       :\r\n")
-                                else:
-                                    of.write("Data URL String       : {0}\r\n".format(row[4]))
-                                if row[5] is None:
-                                    of.write("Sender Name           :\r\n")
-                                else:
-                                    of.write("Sender Name           : {0}\r\n".format(row[5]))
-                                if row[6] is None:
-                                    of.write("Sender Address        :\r\n")
-                                else:
-                                    of.write("Sender Address        : {0}\r\n".format(row[6]))
-                                if row[7] is None:
-                                    of.write("Type Number           :\r\n")
-                                else:
-                                    of.write("Type Number           : {0}\r\n".format(row[7]))
-                                if row[8] is None:
-                                    of.write("Origin Title          :\r\n")
-                                else:
-                                    of.write("Origin Title          : {0}\r\n".format(row[8]))
-                                if row[9] is None:
-                                    of.write("Origin URL String     : \r\n")
-                                else:
-                                    of.write("Origin URL String     : {0}\r\n".format(row[9]))
-                                if row[10] is None:
-                                    of.write("Origin Alias          :\r\n")
-                                else:
-                                    of.write("Origin Alias          : {0}\r\n".format(row[10]))
+                                timestamp = osxripper_time.get_cocoa_seconds(row["LSQuarantineTimeStamp"])
+                                of.write("Event Identifier      : {0}\r\n".format(row["LSQuarantineEventIdentifier"]))
+                                of.write("Timestamp             : {0}\r\n".format(timestamp))
+                                of.write("AgentBundle Identifier: {0}\r\n"
+                                         .format(row["LSQuarantineAgentBundleIdentifier"]))
+                                of.write("Agent Name            : {0}\r\n".format(row["LSQuarantineAgentName"]))
+                                of.write("Data URL String       : {0}\r\n".format(row["LSQuarantineDataURLString"]))
+                                of.write("Sender Name           : {0}\r\n".format(row["LSQuarantineSenderName"]))
+                                of.write("Sender Address        : {0}\r\n".format(row["LSQuarantineSenderAddress"]))
+                                of.write("Type Number           : {0}\r\n".format(row["LSQuarantineTypeNumber"]))
+                                of.write("Origin Title          : {0}\r\n".format(row["LSQuarantineOriginTitle"]))
+                                of.write("Origin URL String     : {0}\r\n".format(row["LSQuarantineOriginURLString"]))
+                                of.write("Origin Alias          : {0}\r\n".format(row["LSQuarantineOriginAlias"]))
                                 of.write("\r\n")
 
                     except sqlite3.Error as e:
