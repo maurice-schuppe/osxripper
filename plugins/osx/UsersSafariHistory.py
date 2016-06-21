9ea753d901bc93e95edd9f063a52b58ce9f6b20f
bolodev <bolodevel@gmail.com>
2016-06-21 20:05:12 +0100

    Clean up of SQL

--- plugins/osx/UsersSafariHistory.py
+++ plugins/osx/UsersSafariHistory.py
@@ -3,6 +3,7 @@ import codecs
 import datetime
 import logging
 import os
+import osxripper_time
 import sqlite3
 import ccl_bplist
 
@@ -67,47 +68,29 @@ class UsersSafariHistory(Plugin):
                          encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             history_db = os.path.join(file, "History.db")
-            query = "SELECT hi.id,hi.url,hi.visit_count,datetime(hv.visit_time + 978307200, 'unixepoch')," \
-                    "hv.title,hv.redirect_source,hv.redirect_destination FROM history_items hi,history_visits hv" \
+            query = "SELECT hi.id,hi.url,hi.visit_count,hv.visit_time," \
+                    "hv.title,hv.redirect_source,hv.redirect_destination " \
+                    "FROM history_items hi,history_visits hv" \
                     " WHERE hi.id = hv.id"
             if os.path.isfile(history_db):
                 of.write("Source File: {0}\r\n\r\n".format(history_db))
                 conn = None
                 try:
                     conn = sqlite3.connect(history_db)
+                    conn.row_factory = sqlite3.Row
                     with conn:    
                         cur = conn.cursor()
                         cur.execute(query)
                         rows = cur.fetchall()
                         for row in rows:
-                            if row[0] is None:
-                                of.write("ID               :\r\n")
-                            else:
-                                of.write("ID               : {0}\r\n".format(row[0]))
-                            if row[1] is None:
-                                of.write("URL              :\r\n")
-                            else:
-                                of.write("URL              : {0}\r\n".format(row[1]))
-                            if row[2] is None:
-                                of.write("Visit Count      :\r\n")
-                            else:
-                                of.write("Visit Count      : {0}\r\n".format(row[2]))
-                            if row[3] is None:
-                                of.write("Visit Time       :\r\n")
-                            else:
-                                of.write("Visit Time       : {0}\r\n".format(row[3]))
-                            if row[4] is None:
-                                of.write("Title            :\r\n")
-                            else:
-                                of.write("Title            : {0}\r\n".format(row[4]))
-                            if row[5] is None:
-                                of.write("Redirect ID      :\r\n")
-                            else:
-                                of.write("Redirect ID      : {0}\r\n".format(row[5]))
-                            if row[6] is None:
-                                of.write("Redirect Dest. ID:\r\n")
-                            else:
-                                of.write("Redirect Dest. ID: {0}\r\n".format(row[6]))
+                            visit_time = osxripper_time.get_cocoa_seconds(row["visit_time"])
+                            of.write("ID               : {0}\r\n".format(row["id"]))
+                            of.write("URL              : {0}\r\n".format(row["url"]))
+                            of.write("Visit Count      : {0}\r\n".format(row["visit_count"]))
+                            of.write("Visit Time       : {0}\r\n".format(visit_time))
+                            of.write("Title            : {0}\r\n".format(row["title"]))
+                            of.write("Redirect ID      : {0}\r\n".format(row["redirect_source"]))
+                            of.write("Redirect Dest. ID: {0}\r\n".format(row["redirect_destination"]))
                             of.write("\r\n")
                 except sqlite3.Error as e:
                     logging.error("{0}".format(e.args[0]))
