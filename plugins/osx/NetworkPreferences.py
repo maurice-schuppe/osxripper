5b4b63e9a2c27ebb2ea8a70e4070942d5cf4b778
bolodev <bolodev@users.noreply.github.com>
2016-07-30 23:51:43 +0100

    Sierra Update

--- plugins/osx/NetworkPreferences.py
+++ plugins/osx/NetworkPreferences.py
@@ -27,13 +27,13 @@ class NetworkPreferences(Plugin):
     
     def parse(self):
         """
-        Parse /Library/Preferences/SystemConfiguration/NetworkInterfaces.plist
+        Parse /Library/Preferences/SystemConfiguration/preferences.plist
         """
         with codecs.open(os.path.join(self._output_dir, self._output_file), "a", encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             plist_file = os.path.join(self._input_dir, "Library", "Preferences", "SystemConfiguration", self._data_file)
             of.write("Source File: {0}\r\n\r\n".format(plist_file))
-            if self._os_version in ["el_capitan", "yosemite", "mavericks", "mountain_lion", "lion"]:
+            if self._os_version in ["sierra", "el_capitan", "yosemite", "mavericks", "mountain_lion", "lion"]:
                 if os.path.isfile(plist_file):
                     with open(plist_file, "rb") as pl:
                         plist = plistlib.load(pl)
