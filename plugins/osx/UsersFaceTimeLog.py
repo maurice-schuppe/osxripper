6e4d2addc6f16fd85c963a3177f6ce5837aa0262
bolodev <bolodev@users.noreply.github.com>
2016-07-31 21:44:15 +0100

    Sierra Update

--- plugins/osx/UsersFaceTimeLog.py
+++ plugins/osx/UsersFaceTimeLog.py
@@ -50,7 +50,8 @@ class UsersFaceTimeLog(Plugin):
         with codecs.open(os.path.join(self._output_dir, "Users_" + username + ".txt"), "a", encoding="utf-8") as of:
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             of.write("Source File: {0}\r\n\r\n".format(file))
-            if self._os_version in ["el_capitan", "yosemite", "mavericks", "mountain_lion", "lion", "snow_leopard"]:
+            if self._os_version in ["sierra", "el_capitan", "yosemite", "mavericks",
+                                    "mountain_lion", "lion", "snow_leopard"]:
                 with codecs.open(file, "r", encoding="utf-8") as ft:
                     for line in ft.readlines():
                         if "Received invite push from" in line:
