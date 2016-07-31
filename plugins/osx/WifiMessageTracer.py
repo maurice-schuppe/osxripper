8d2bff8007c6a546be1401157bd45077467c94a7
bolodev <bolodev@users.noreply.github.com>
2016-07-31 22:25:40 +0100

    Sierra Update

--- plugins/osx/WifiMessageTracer.py
+++ plugins/osx/WifiMessageTracer.py
@@ -33,7 +33,7 @@ class WifiMessageTracer(Plugin):
             of.write("="*10 + " " + self._name + " " + "="*10 + "\r\n")
             plist_file = os.path.join(self._input_dir, "Library", "Preferences", "SystemConfiguration", self._data_file)
             of.write("Source File: {0}\r\n\r\n".format(plist_file))
-            if self._os_version in ["el_capitan", "yosemite"]:
+            if self._os_version in ["sierra", "el_capitan", "yosemite"]:
                 if os.path.isfile(plist_file):
                     with open(plist_file, "rb") as pl:
                         plist = plistlib.load(pl)
@@ -46,7 +46,8 @@ class WifiMessageTracer(Plugin):
                         if "InternalAssociationSSIDMap" in plist:
                             of.write("InternalAssociationSSIDMap:\r\n")
                             for iasm_key in plist["InternalAssociationSSIDMap"]:
-                                of.write("\t{0}: {1}\r\n".format(iasm_key, plist["InternalAssociationSSIDMap"][iasm_key]))
+                                of.write("\t{0}: {1}\r\n"
+                                         .format(iasm_key, plist["InternalAssociationSSIDMap"][iasm_key]))
                             of.write("\r\n")
                         if "LastSubmissionTimestamp" in plist:
                             of.write("Last Submission Timestamp: {0}\r\n".format(plist["LastSubmissionTimestamp"]))
